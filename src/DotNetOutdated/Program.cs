273f776302db1c1ecade659ea037073fee4f9351
Jerrie Pelser <jerrie@jerriepelser.com>
2018-05-17 09:34:16 +0700

    Refactoring

--- src/DotNetOutdated/Program.cs
+++ src/DotNetOutdated/Program.cs
@@ -176,17 +176,4 @@ namespace DotNetOutdated
             return manager.Projects.Values.Select(pa => pa.Project).ToList();
         }
     }
-
-    public static class ReportingExtensions
-    {
-        public static int[] DetermineColumnWidths(this List<ReportedPackage> packages)
-        {
-            List<int> columnWidths = new List<int>();
-            columnWidths.Add(packages.Select(p => p.Name).Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur).Length);
-            columnWidths.Add(packages.Select(p => p.ReferencedVersion.ToString()).Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur).Length);
-            columnWidths.Add(packages.Select(p => p.LatestVersion.ToString()).Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur).Length);
-
-            return columnWidths.ToArray();
-        }
-    }
 }
---
+++ src/DotNetOutdated/ReportingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DotNetOutdated
+{
+    public static class ReportingExtensions
+    {
+        public static int[] DetermineColumnWidths(this List<ReportedPackage> packages)
+        {
+            List<int> columnWidths = new List<int>();
+            columnWidths.Add(packages.Select(p => p.Name).Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur).Length);
+            columnWidths.Add(packages.Select(p => p.ReferencedVersion.ToString()).Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur).Length);
+            columnWidths.Add(packages.Select(p => p.LatestVersion.ToString()).Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur).Length);
+
+            return columnWidths.ToArray();
+        }
+    }
+}
