bf8b63ff129f4835503cc0c663792f4063080fc6
jrr <john.ruble@atomicobject.com>
2018-05-31 09:58:13 -0400

    support fsproj

--- test/DotNetOutdated.Tests/ProjectDiscoveryServiceTests.cs
+++ test/DotNetOutdated.Tests/ProjectDiscoveryServiceTests.cs
@@ -13,6 +13,7 @@ namespace DotNetOutdated.Tests
         private const string Solution2 = @"c:\path\solution2.sln";
         private const string Project1 = @"c:\path\project1.csproj";
         private const string Project2 = @"c:\path\project2.csproj";
+        private const string Project3 = @"c:\path\project3.fsproj";
         private const string NonProjectFile = @"c:\path\file.cs";
 
         [Fact]
@@ -51,7 +52,7 @@ namespace DotNetOutdated.Tests
         }
         
         [Fact]
-        public void SingleProject_ReturnsProject()
+        public void SingleProject_ReturnsCsProject()
         {
             // Arrange
             var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
@@ -65,6 +66,23 @@ namespace DotNetOutdated.Tests
             
             // Assert
             Assert.Equal(project, Project1);
+        }
+        
+        [Fact]
+        public void SingleProject_ReturnsFsProject()
+        {
+            // Arrange
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                { Project3, MockFileData.NullObject}
+            }, Path);
+            var projectDiscoveryService = new ProjectDiscoveryService(fileSystem);
+            
+            // Act
+            string project = projectDiscoveryService.DiscoverProject(Path);
+            
+            // Assert
+            Assert.Equal(project, Project3);
         }
 
         [Fact]
