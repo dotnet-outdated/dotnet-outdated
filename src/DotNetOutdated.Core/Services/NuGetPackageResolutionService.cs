98ecdc4620b487804d125b285859b7dd8af2e2ff
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2023-01-10 10:55:39 +0100

    Fix NRE (#365)

--- src/DotNetOutdated.Core/Services/NuGetPackageResolutionService.cs
+++ src/DotNetOutdated.Core/Services/NuGetPackageResolutionService.cs
@@ -49,7 +49,7 @@ namespace DotNetOutdated.Core.Services
             if (versionLock == VersionLock.Minor)
                 floatingBehaviour = includePrerelease ? NuGetVersionFloatBehavior.PrereleasePatch : NuGetVersionFloatBehavior.Patch;
 
-            string releasePrefix = null;
+            string releasePrefix = string.Empty;
             if (referencedVersion.IsPrerelease)
                 releasePrefix = referencedVersion.ReleaseLabels.First(); // TODO Not sure exactly what to do for this bit
 
