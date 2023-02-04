fe791b0db1266d4402436f9e9cff3ee47121eb93
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2023-02-04 16:42:34 +0100

    Fix code analysis

--- src/DotNetOutdated.Core/Services/ProjectDiscoveryService.cs
+++ src/DotNetOutdated.Core/Services/ProjectDiscoveryService.cs
@@ -1,13 +1,14 @@
-﻿using System;
+﻿using DotNetOutdated.Core.Exceptions;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.IO.Abstractions;
 using System.Linq;
-using DotNetOutdated.Core.Exceptions;
 
 namespace DotNetOutdated.Core.Services
 {
-    public class ProjectDiscoveryService : IProjectDiscoveryService
+    public sealed class ProjectDiscoveryService : IProjectDiscoveryService
     {
         private readonly IFileSystem _fileSystem;
 
@@ -15,14 +16,14 @@ namespace DotNetOutdated.Core.Services
         {
             _fileSystem = fileSystem;
         }
-        
+
         public IList<string> DiscoverProjects(string path, bool recursive = false)
         {
             if (!(_fileSystem.File.Exists(path) || _fileSystem.Directory.Exists(path)))
-                throw new CommandValidationException(string.Format(Resources.ValidationErrorMessages.DirectoryOrFileDoesNotExist, path));
+                throw new CommandValidationException(string.Format(CultureInfo.InvariantCulture, Resources.ValidationErrorMessages.DirectoryOrFileDoesNotExist, path));
 
             var fileAttributes = _fileSystem.File.GetAttributes(path);
-            
+
             // If a directory was passed in, search for a .sln or .csproj file
             if (fileAttributes.HasFlag(FileAttributes.Directory))
             {
@@ -36,38 +37,40 @@ namespace DotNetOutdated.Core.Services
                         return recursiveProjectFiles;
 
                     // At this point the path contains no solutions or projects, so throw an exception
-                    throw new CommandValidationException(string.Format(Resources.ValidationErrorMessages.DirectoryDoesNotContainSolutionsOrProjects, path));
+                    throw new CommandValidationException(string.Format(CultureInfo.InvariantCulture, Resources.ValidationErrorMessages.DirectoryDoesNotContainSolutionsOrProjects, path));
                 }
 
                 // Search for solution(s)
                 var solutionFiles = _fileSystem.Directory.GetFiles(path, "*.sln");
                 if (solutionFiles.Length == 1)
-                    return new[] {_fileSystem.Path.GetFullPath(solutionFiles[0])};
-                
+                    return new[] { _fileSystem.Path.GetFullPath(solutionFiles[0]) };
+
                 if (solutionFiles.Length > 1)
-                    throw new CommandValidationException(string.Format(Resources.ValidationErrorMessages.DirectoryContainsMultipleSolutions, path));
-                
+                    throw new CommandValidationException(string.Format(CultureInfo.InvariantCulture, Resources.ValidationErrorMessages.DirectoryContainsMultipleSolutions, path));
+
                 // We did not find any solutions, so try and find individual projects
                 var projectFiles = _fileSystem.Directory.GetFiles(path, "*.csproj").Concat(_fileSystem.Directory.GetFiles(path, "*.fsproj")).ToArray();
                 if (projectFiles.Length == 1)
-                    return new[] {_fileSystem.Path.GetFullPath(projectFiles[0])};
-                
+                    return new[] { _fileSystem.Path.GetFullPath(projectFiles[0]) };
+
                 if (projectFiles.Length > 1)
-                    throw new CommandValidationException(string.Format(Resources.ValidationErrorMessages.DirectoryContainsMultipleProjects, path));
+                    throw new CommandValidationException(string.Format(CultureInfo.InvariantCulture, Resources.ValidationErrorMessages.DirectoryContainsMultipleProjects, path));
 
                 // At this point the path contains no solutions or projects, so throw an exception
-                throw new CommandValidationException(string.Format(Resources.ValidationErrorMessages.DirectoryDoesNotContainSolutionsOrProjects, path));
+                throw new CommandValidationException(string.Format(CultureInfo.InvariantCulture, Resources.ValidationErrorMessages.DirectoryDoesNotContainSolutionsOrProjects, path));
             }
 
             // If a .sln or .csproj file was passed, just return that
-            if ((string.Compare(_fileSystem.Path.GetExtension(path), ".sln", StringComparison.OrdinalIgnoreCase) == 0) ||
-                (string.Compare(_fileSystem.Path.GetExtension(path), ".csproj", StringComparison.OrdinalIgnoreCase) == 0) ||
-                (string.Compare(_fileSystem.Path.GetExtension(path), ".fsproj", StringComparison.OrdinalIgnoreCase) == 0) ||
-                (string.Compare(_fileSystem.Path.GetExtension(path), ".slnf", StringComparison.OrdinalIgnoreCase) == 0))
-                return new[] { _fileSystem.Path.GetFullPath(path)};
+            if ((string.Equals(_fileSystem.Path.GetExtension(path), ".sln", StringComparison.OrdinalIgnoreCase)) ||
+                (string.Equals(_fileSystem.Path.GetExtension(path), ".csproj", StringComparison.OrdinalIgnoreCase)) ||
+                (string.Equals(_fileSystem.Path.GetExtension(path), ".fsproj", StringComparison.OrdinalIgnoreCase)) ||
+                (string.Equals(_fileSystem.Path.GetExtension(path), ".slnf", StringComparison.OrdinalIgnoreCase)))
+            {
+                return new[] { _fileSystem.Path.GetFullPath(path) };
+            }
 
             // At this point, we know the file passed in is not a valid project or solution
-            throw new CommandValidationException(string.Format(Resources.ValidationErrorMessages.FileNotAValidSolutionOrProject, path));
+            throw new CommandValidationException(string.Format(CultureInfo.InvariantCulture, Resources.ValidationErrorMessages.FileNotAValidSolutionOrProject, path));
         }
     }
 }
