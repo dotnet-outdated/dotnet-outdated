426d367c719b4e7943892b5713fea5bfddc4d934
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2023-02-04 16:50:05 +0100

    Organize the colors used

---
+++ src/DotNetOutdated/ReportingColors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DotNetOutdated
+{
+    public sealed class ReportingColors
+    {
+        public ConsoleColor ProjectName { get; } = ConsoleColor.Blue;
+        public ConsoleColor TargetFrameworkName { get; } = ConsoleColor.Cyan;
+        public ConsoleColor PackageName { get; } = ConsoleColor.Magenta;
+
+        public ConsoleColor MajorVersionUpgrade { get; } = ConsoleColor.Red;
+        public ConsoleColor MinorVersionUpgrade { get; } = ConsoleColor.Yellow;
+        public ConsoleColor PatchVersionUpgrade { get; } = ConsoleColor.Green;
+
+        public ConsoleColor UpgradeSuccess { get; } = ConsoleColor.Green;
+        public ConsoleColor UpgradeFailure { get; } = ConsoleColor.Red;
+    }
+}
