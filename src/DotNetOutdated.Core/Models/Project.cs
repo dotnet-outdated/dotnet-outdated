6f10d4f9cbac86b0836fea4ce8a1dbc2b76c3e8f
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2023-02-04 16:49:30 +0100

    Fix code analysis

--- src/DotNetOutdated.Core/Models/Project.cs
+++ src/DotNetOutdated.Core/Models/Project.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using NuGet.Frameworks;
+﻿using NuGet.Frameworks;
 using NuGet.Versioning;
+using System;
+using System.Collections.Generic;
 
 namespace DotNetOutdated.Core.Models
 {
