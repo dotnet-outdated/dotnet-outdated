f1d026604a43ff9e90b78187b872386edc0dfb4c
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2023-02-04 16:39:01 +0100

    Add necessary structures to maintain compatibility after upgrading System.IO.Abstractions

---
+++ test/DotNetOutdated.Tests/Singletons.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO.Abstractions.TestingHelpers;
+
+namespace DotNetOutdated.Tests
+{
+    internal static class Singletons
+    {
+        public static MockFileData NullObject { get; } = new(string.Empty)
+        {
+            LastWriteTime = new DateTime(1601, 01, 01, 00, 00, 00, DateTimeKind.Utc),
+            LastAccessTime = new DateTime(1601, 01, 01, 00, 00, 00, DateTimeKind.Utc),
+            CreationTime = new DateTime(1601, 01, 01, 00, 00, 00, DateTimeKind.Utc),
+        };
+    }
+}
