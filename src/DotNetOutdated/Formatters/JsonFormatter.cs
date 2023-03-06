bec10684294c7cc27524e784b23bc981cd7d9422
Yennefer of Vengerberg <5850742+yenneferofvengerberg@users.noreply.github.com>
2023-03-06 16:18:55 +0100

    Fix code analysis

--- src/DotNetOutdated/Formatters/JsonFormatter.cs
+++ src/DotNetOutdated/Formatters/JsonFormatter.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 
 namespace DotNetOutdated.Formatters;
@@ -26,7 +27,7 @@ internal class JsonFormatter : IOutputFormatter
         internal static string GetTextReportLine(AnalyzedProject project, AnalyzedTargetFramework targetFramework, AnalyzedDependency dependency)
         {
             var upgradeSeverity = Enum.GetName(typeof(DependencyUpgradeSeverity), dependency.UpgradeSeverity);
-            return string.Format("{0};{1};{2};{3};{4};{5}",
+            return string.Format(CultureInfo.InvariantCulture, "{0};{1};{2};{3};{4};{5}",
                 project.Name,
                 targetFramework.Name,
                 dependency.Name,
