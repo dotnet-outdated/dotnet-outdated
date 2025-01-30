b25b77fa74d886ab7233b4b1a20a9297b2b9993b
Edward Miller <symbiogenisis@outlook.com>
2025-01-29 21:56:52 -0600

    optimize column width calculations

--- src/DotNetOutdated/ReportingExtensions.cs
+++ src/DotNetOutdated/ReportingExtensions.cs
@@ -11,10 +11,11 @@ namespace DotNetOutdated
         {
             ArgumentNullException.ThrowIfNull(packages);
 
-            return [
-                packages.Select(p => p.Description).Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur).Length,
-                packages.Select(p => p.ResolvedVersion?.ToString() ?? "").Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur).Length,
-                packages.Select(p => p.LatestVersion?.ToString() ?? "").Aggregate("", (max, cur) => max.Length > cur.Length ? max : cur).Length
+            return
+            [
+                packages.Max(p => p.Description.Length),
+                packages.Max(p => p.ResolvedVersion?.ToString().Length ?? 0),
+                packages.Max(p => p.LatestVersion?.ToString().Length ?? 0)
             ];
         }
     }
